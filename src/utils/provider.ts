a61281bc61eca5277ec2e876aebb379c373d360e
Mr yang <ywl1049728030@163.com>
2022-09-02 11:01:17 +0800

    Add files via upload
    project init

---
+++ src/utils/provider.ts
@@ -0,0 +1,24 @@
+import {
+  JsonRpcBatchProvider,
+  StaticJsonRpcProvider
+} from '@ethersproject/providers';
+
+const providers = {};
+const batchedProviders = {};
+
+export default function getProvider(network) {
+  const url = `https://brovider.xyz/${network}`;
+  if (!providers[network])
+    providers[network] = new StaticJsonRpcProvider({ url, timeout: 25000 });
+  return providers[network];
+}
+
+export function getBatchedProvider(network) {
+  const url = `https://brovider.xyz/${network}`;
+  if (!batchedProviders[network])
+    batchedProviders[network] = new JsonRpcBatchProvider({
+      url,
+      timeout: 25000
+    });
+  return batchedProviders[network];
+}
---
+++ test/schema.ts
@@ -0,0 +1,32 @@
+const snapshot = require('../');
+const space = require('./examples/space.json');
+const proposal = require('./examples/proposal.json');
+const vote = require('./examples/vote.json');
+const profile = require('./examples/profile.json');
+
+const isValidSpace = snapshot.utils.validateSchema(
+  snapshot.schemas.space,
+  space
+);
+console.log('Space', isValidSpace);
+
+const isValidProposal = snapshot.utils.validateSchema(
+  snapshot.schemas.proposal,
+  proposal
+);
+console.log('Proposal', isValidProposal);
+
+const isValidVote = snapshot.utils.validateSchema(snapshot.schemas.vote, vote);
+console.log('Vote', isValidVote);
+
+const isValidProfile = snapshot.utils.validateSchema(
+  snapshot.schemas.profile,
+  profile
+);
+console.log('Profile', isValidProfile);
+
+const isValidZodiac = snapshot.utils.validateSchema(
+  snapshot.schemas.zodiac,
+  space
+);
+console.log('Zodiac', isValidZodiac);
